0f3fc9bd7d10731c60a57b088f8f13a0c826bbbb
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2024-02-29 17:50:59 +0330

    improve code

--- src/Traits/ConversionTrait.php
+++ src/Traits/ConversionTrait.php
@@ -11,6 +11,7 @@
      * and number fields to Persian numbers.
 	 * for convert date to persian, add protected variable named dates array type
 	 * for price format, add protected variable named prices array type
+	 * @property array $dates
      */
 	trait ConversionTrait
 	{
