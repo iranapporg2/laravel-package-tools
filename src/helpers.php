42d0916ba09d9ba5cd17ff7e72f66cbfd8d3a199
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2024-10-28 12:22:51 +0330

    refactor function

--- src/helpers.php
+++ src/helpers.php
@@ -4,37 +4,42 @@
 	use iranapp\Tools\Helpers\ConversionHelper;
 
 	if (!function_exists('isActiveRoute')) {
-        function isActiveRoute($routePatterns, $output = "active")
-        {
-            if (!is_array($routePatterns)) {
-                $routePatterns = [$routePatterns];
-            }
+		function isActiveRoute($routePatterns, $output = "active", $queryParameters = [])
+		{
+			if (!is_array($routePatterns)) {
+				$routePatterns = [$routePatterns];
+			}
 
-            $currentRouteName = Route::currentRouteName();
+			$currentRouteName = Route::currentRouteName();
+			$currentQueryParameters = request()->query(); // Get the current query parameters
 
-            foreach ($routePatterns as $pattern) {
-                // Check if the pattern ends with '*'
-                if (str_ends_with($pattern, '*')) {
-                    // Get the prefix by removing the '*' at the end
-                    $prefix = rtrim($pattern, '*');
-                    // Check if the current route name starts with the prefix
-                    if (str_starts_with($currentRouteName, $prefix)) {
-                        return $output;
-                    }
-                } elseif ($currentRouteName === $pattern) {
-                    return $output;
-                }
-            }
+			foreach ($routePatterns as $pattern) {
+				// Check if the pattern ends with '*'
+				if (str_ends_with($pattern, '*')) {
+					// Get the prefix by removing the '*' at the end
+					$prefix = rtrim($pattern, '*');
+					// Check if the current route name starts with the prefix
+					if (str_starts_with($currentRouteName, $prefix)) {
+						if (empty($queryParameters) || array_intersect_assoc($queryParameters, $currentQueryParameters)) {
+							return $output;
+						}
+					}
+				} elseif ($currentRouteName === $pattern) {
+					if (empty($queryParameters) || array_intersect_assoc($queryParameters, $currentQueryParameters)) {
+						return $output;
+					}
+				}
+			}
 
-            return '';
-        }
-    }
+			return '';
+		}
+	}
 
     function successBack($message = null) {
         return back()->with('message',$message ?? trans('custom.message.success'));
     }
 
-    function failBack($message = null) {
+    function errorBack($message = null) {
         return back()->with('message',$message ?? trans('custom.message.failed'));
     }
 
