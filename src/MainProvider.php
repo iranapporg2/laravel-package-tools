6d5196270b110f8c58c19cfd1317ac01b9956291
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2024-11-04 16:19:07 +0330

    refactor function

--- src/MainProvider.php
+++ src/MainProvider.php
@@ -17,6 +17,10 @@
 			],'laravel-assets');
 
 			$this->publishes([
+				__DIR__.'/View/Components' => base_path('app/View/Components'),
+			],'laravel-assets');
+
+			$this->publishes([
 				__DIR__.'/other/database/migrations'  => database_path('laravel-assets'),
 			],'laravel-assets');
 
