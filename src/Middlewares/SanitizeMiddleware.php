cac5312c6442c4cff204afc2d58eae4c9a5832b9
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2024-11-28 16:19:30 +0330

    optimize

--- src/Middlewares/SanitizeMiddleware.php
+++ src/Middlewares/SanitizeMiddleware.php
@@ -1,38 +1,39 @@
 <?php
 
-    namespace iranapp\Tools\Middlewares;
+	namespace iranapp\Tools\Middlewares;
 
-    use Closure;
-    use Hekmatinasser\Verta\Verta;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Str;
-    use Symfony\Component\HttpFoundation\Response;
+	use Closure;
+	use Illuminate\Http\Request;
+	use Symfony\Component\HttpFoundation\Response;
 
-    class SanitizeMiddleware {
-        /**
-         * Handle an incoming request.
-         *
-         * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
-         */
-        public function handle(Request $request, Closure $next): Response {
+	class SanitizeMiddleware {
+		/**
+		 * Handle an incoming request.
+		 *
+		 * @param \Closure(Request): (Response) $next
+		 * @param mixed ...$fields
+		 */
+		public function handle(Request $request, Closure $next, ...$fields): Response {
+			// Get all request variables
+			$variables = $request->all();
 
-            $variables = $request->all();
+			foreach ($variables as $variable => $temp) {
+				// If no fields specified, sanitize all; otherwise, only sanitize specified fields
+				if (empty($fields) || in_array($variable, $fields)) {
+					$temp = conversion()->sanitize($temp);
 
-            foreach ($variables as $variable => $temp) {
+					// Check if the variable is a date and convert if necessary
+					if (preg_match('|^\d{4}/\d{1,2}/\d{1,2}$|', $temp)) {
+						$temp = conversion()->gregorian($temp);
+					}
 
-				$temp = conversion()->sanitize($temp);
+					// Merge sanitized value back into the request
+					$request->merge([
+						$variable => $temp,
+					]);
+				}
+			}
 
-				if (preg_match('|^\d{4}/\d{1,2}/\d{1,2}$|', $temp))
-					$temp = conversion()->gregorian($temp);
-
-				$request->merge([
-					$variable => $temp
-				]);
-
-            }
-
-            return $next($request);
-
-        }
-
-    }
+			return $next($request);
+		}
+	}
