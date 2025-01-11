3a169d247246de7ba51ec73dab7ea4770a9bc919
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2025-01-11 15:59:35 +0330

    fix bug

--- src/Traits/ApiResponseTrait.php
+++ src/Traits/ApiResponseTrait.php
@@ -7,25 +7,29 @@
 
 	trait ApiResponseTrait {
 
-        public function json($status,$message = '',$data = null,$statusCode = 200): \Illuminate\Http\JsonResponse {
+		public function initilizeApiResponse() {
 
-            $message = [
-                'status' => $status,
-                'message' => $message
-            ];
+			if (!method_exists($this, 'json')) {
+				$this->json = function($status, $message = '', $data = null, $statusCode = 200): \Illuminate\Http\JsonResponse {
+					$message = [
+						'status' => $status,
+						'message' => $message
+					];
 
-            if (!is_null($data))
-                $message['data'] = $data;
+					if (!is_null($data))
+						$message['data'] = $data;
 
-            return response()->json($message,$statusCode,[],JSON_UNESCAPED_UNICODE);
+					return response()->json($message, $statusCode, [], JSON_UNESCAPED_UNICODE);
+				};
+			}
 
-        }
+		}
 
 		public function failedValidation(Validator $validator) {
 
 			throw new HttpResponseException(response()->json([
 				'status' => false,
-				'message' => ['validation' => $validator->errors()],
+				'message' => ['validation' => $validator->errors()->all()],
 			]));
 
 		}
