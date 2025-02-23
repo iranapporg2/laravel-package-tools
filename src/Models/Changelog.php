5a2571abe27a21ba4f299f4fe0d197708def5b28
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2025-02-23 11:58:42 +0330

    fix bug

---
+++ src/Models/Changelog.php
@@ -0,0 +1,12 @@
+<?php
+
+	namespace iranapp\Tools\Models;
+
+	use Illuminate\Database\Eloquent\Factories\HasFactory;
+	use Illuminate\Database\Eloquent\Model;
+
+	class Changelog extends Model {
+		use HasFactory;
+		public $guarded = [];
+
+	}
