d585820bc9d2b33df81fec537fc571bbe65b311f
Omid Aghakhani <157590431+iranapporg2@users.noreply.github.com>
2025-04-07 17:54:45 +0330

    fix bug

--- src/Rules/BooleanRule.php
+++ src/Rules/BooleanRule.php
@@ -1,23 +1,36 @@
 <?php
 
 	namespace iranapp\Tools\Rules;
+
 	use Closure;
 	use Illuminate\Contracts\Validation\ValidationRule;
+	use Illuminate\Contracts\Validation\ValidatorAwareRule;
+	use Illuminate\Validation\Validator;
 
-	class BooleanRule implements ValidationRule
+	class BooleanRule implements ValidationRule,ValidatorAwareRule
 	{
+
+		protected Validator $validator;
+
 		/**
 		 * Run the validation rule.
-		 *
-		 * @param  string  $attribute
-		 * @param  mixed  $value
-		 * @param  \Closure  $fail
-		 * @return void
 		 */
-		public function validate(string $attribute, mixed $value, \Closure $fail): void
+		public function validate(string $attribute, mixed $value, Closure $fail): void
 		{
-			$value = conversion()->sanitize($value);
-			if (!in_array($value,['0','1','false','true']))
-				$fail(trans('validation.in',[trans('custom.value')]));
+			$allowed = [1, 0, true, false, '1', '0', 'true', 'false'];
+
+			if (!in_array($value, $allowed, true)) {
+				$fail("The $attribute must be a boolean (1, 0, true, false).");
+			}
+
+			$converted = filter_var($value, FILTER_VALIDATE_BOOLEAN) ? 1 : 0;
+			$this->validator->setValue($attribute, $converted);
+
 		}
+
+		public function setValidator(Validator $validator) {
+			$this->validator = $validator;
+			return $this;
+		}
+
 	}
